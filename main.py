56db371c7e038ed5b0f230f4891990ed02f20781
c1edue <110680614+c1edue@users.noreply.github.com>
2022-09-24 17:46:44 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+from art import tprint
+import math, os
+P = 3.14
+os.system("cls")
+tprint('L=R*P*R')
+print('this software is made to calculate circles')
+def main():
+	print('')
+	R = input('write the radius of the circle:\nR = ')
+	try:
+		int(R)
+		print('processing...')
+	except ValueError:
+		print(f'"{R}", is not a number')
+		exit()
+	L = (P*2*float(R))
+	S = (float(R)**2*P)
+	print(f'L: {L}\nS: {S}')
+
+if __name__ == '__main__':
+	main()
